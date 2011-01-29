8d0bdb876ab4ac27494279c45a5ccd04ff114eeb
bcsanches <bcsanches@localhost>
2011-01-29 00:21:32 -0200

    Versao inicial do lobo

--- jogo_atores.h
+++ jogo_atores.h
@@ -4,10 +4,26 @@
 #include <c2d2/chien2d2mapa.h>
 
 // A enumeração com o tipo dos personagens
-enum { DARKPHOENIX , BOLA};
+enum 
+{ 
+	DARKPHOENIX, 
+	BOLA,
+	LOBO,
+	RATO,
+	CORUJA,
+	MORCEGO
+};
 
 // A enumeração das marcas
 
-enum {MARCA_BOLA_DIREITA = C2D2M_PROG, MARCA_BOLA_ESQUERDA};
+enum 
+{
+	MARCA_BOLA_DIREITA = C2D2M_PROG, 
+	MARCA_BOLA_ESQUERDA,
+	MARCA_LOBO,
+	MARCA_RATO,
+	MARCA_CORUJA,
+	MARCA_MORCEGO
+};
 
 #endif
