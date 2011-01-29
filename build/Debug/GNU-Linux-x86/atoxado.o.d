0b15684559d58f9d3af94a659fa73408e0caef3b
bcsanches <bcsanches@localhost>
2011-01-29 12:17:19 -0200

    Tratamento de colis?o diferenciado para gotas e itens

---
+++ build/Debug/GNU-Linux-x86/atoxado.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-Linux-x86/atoxado.o: atoxado.cpp jogo_atores.h atoxado.h
+
+jogo_atores.h:
+
+atoxado.h:
---
+++ build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,14 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp jogo_atores.h atoxado.h \
+ jogo_bola.h lobo.h rato.h jogo_coruja.h
+
+jogo_atores.h:
+
+atoxado.h:
+
+jogo_bola.h:
+
+lobo.h:
+
+rato.h:
+
+jogo_coruja.h:
