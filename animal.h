859ae1897109e4507c91bf4e62ae1b0d47ddb5d0
Paulo Vinicius Wolski Radtke <pvwradtke@gmail.com>
2011-01-30 12:46:51 -0200

    Mapa

---
+++ animal.h
@@ -0,0 +1,39 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <c2d2/ator.h>
+
+// Enumera��o dos estados dos animais do jogo
+enum 
+{
+	ANIMAL_ANDANDO=ATOR_PROG_INI, 
+	ANIMAL_MORRENDO,
+	ANIMAL_PREPARA_ATAQUE,
+	ANIMAL_ATACANDO,
+	ANIMAL_ESCONDIDO,
+	ANIMAL_PROG_INI
+};
+
+enum
+{
+	ANIM_ANDANDO_DIREITA,
+	ANIM_ANDANDO_ESQUERDA,
+	ANIM_MORRENDO,
+	ANIM_ESCONDIDO
+};
+
+struct InfoAnimal
+{
+	int velocidade;
+	int velocidadeAtaque;
+
+	int tempoBote;
+	int tempoAtaque;
+	int tempoEsconderijo;
+
+        int sons;
+};
+
+bool AtualizaAnimal(Ator *a, InfoAnimal *info, unsigned int mapa);
+
+#endif
