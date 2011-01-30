993bcea4d180962413e6c24ad1edd5e398a11094
Paulo Vinicius Wolski Radtke <pvwradtke@gmail.com>
2011-01-30 02:54:34 -0200

    son em personagens.

---
+++ gota.cpp
@@ -0,0 +1,132 @@
+#include "gota.h"
+
+#include <c2d2/ator.h>
+#include <stdlib.h>
+
+#include "jogo_atores.h"
+
+#define TEMPO_CRESCIMENTO_GOTA 25
+
+#define INTERVALO_MIN 10
+#define INTERVALO_MAX 100
+#define INTERVALO_REGULAR 30
+
+enum 
+{
+	GOTA_PENDURADA = ATOR_PROG_INI,
+	GOTA_ESCONDIDA,
+	GOTA_CAINDO
+};
+
+Animacao animGota[] ={
+	// Ordem: número de quadros, tempo entre os quadros, vetor com a seqüência de quadros
+	//  direita: 0
+	{1, 1, {1}},
+	//  esquerda: 1
+	{1, 1, {4}},
+	// ATOXADO_MORRENDO: 9
+	{1, 1, {12}}
+};
+
+static bool AtualizaGota(Ator *a, unsigned int mapa);
+
+bool CarregaGota()
+{
+	return ATOR_CarregaAtorEstatico(
+		GOTA, 
+		"imagens/darkphoenix.png", 
+		32, 
+		42, 
+		4, 
+		2,
+		24, 
+		39, 
+		animGota, 
+		false, 
+		0, 
+		0, 
+		&AtualizaGota
+	);
+}
+
+static int Intervalo()
+{
+	return INTERVALO_MIN + (rand() % INTERVALO_MAX);
+}
+
+static bool AtualizaGota(Ator *a, unsigned int mapa)
+{
+	Evento ev;
+	switch(a->estado.estado)
+	{
+		case ATOR_NASCENDO:
+			// Muda para o estado adequado			
+			a->gravidade = false;
+			a->aux_real[0] = a->x;
+			a->aux_real[1] = a->y;
+			ATOR_TrocaEstado(a, GOTA_ESCONDIDA, false);
+			break;
+
+		case GOTA_ESCONDIDA:
+			if(a->estado.subestado == ESTADO_INICIO)
+			{
+				a->temporizadores[0] = Intervalo();
+				a->estado.subestado = ESTADO_RODANDO;
+				ATOR_TrocaAnimacao(a, 0);
+			}
+			while(ATOR_ProximoEvento(a, &ev))
+			{
+				switch(ev.tipoEvento)
+				{
+					case EVT_TEMPO:
+						ATOR_TrocaEstado(a, GOTA_PENDURADA, false);
+						break;
+				}
+			}
+			break;
+
+		case GOTA_PENDURADA:
+			if(a->estado.subestado == ESTADO_INICIO)
+			{
+				a->temporizadores[0] = TEMPO_CRESCIMENTO_GOTA;
+				a->estado.subestado = ESTADO_RODANDO;
+
+				ATOR_TrocaAnimacao(a, 1);
+			}
+			while(ATOR_ProximoEvento(a, &ev))
+			{
+				switch(ev.tipoEvento)
+				{
+					case EVT_TEMPO:
+						ATOR_TrocaEstado(a, GOTA_CAINDO, false);
+						break;
+				}
+			}
+			break;
+
+		case GOTA_CAINDO:
+			if(a->estado.subestado == ESTADO_INICIO)
+			{
+				a->gravidade = true;
+				a->estado.subestado = ESTADO_RODANDO;
+			}
+
+			while(ATOR_ProximoEvento(a, &ev))
+			{
+				switch(ev.tipoEvento)
+				{
+					case EVT_COLIDIU_PAREDE:	
+					case EVT_COLIDIU_PERSONAGEM:
+						a->gravidade = 0;
+						a->x = a->aux_real[0];
+						a->y = a->aux_real[1];
+						ATOR_TrocaEstado(a, GOTA_ESCONDIDA, false);
+						break;
+				}
+			}
+
+			break;
+	}
+
+	return true;
+}
---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,139 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Release
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/item_madeira.o \
+	${OBJECTDIR}/cachoeira.o \
+	${OBJECTDIR}/jogo_bola.o \
+	${OBJECTDIR}/lobo.o \
+	${OBJECTDIR}/jogo_morcego.o \
+	${OBJECTDIR}/gota.o \
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/jogo_coruja.o \
+	${OBJECTDIR}/rato.o \
+	${OBJECTDIR}/animal.o \
+	${OBJECTDIR}/atoxado.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/GNU-Linux-x86/torquere
+
+dist/Release/GNU-Linux-x86/torquere: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/GNU-Linux-x86
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/torquere ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/item_madeira.o: nbproject/Makefile-${CND_CONF}.mk item_madeira.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/item_madeira.o item_madeira.cpp
+
+${OBJECTDIR}/cachoeira.o: nbproject/Makefile-${CND_CONF}.mk cachoeira.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/cachoeira.o cachoeira.cpp
+
+${OBJECTDIR}/jogo_bola.o: nbproject/Makefile-${CND_CONF}.mk jogo_bola.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/jogo_bola.o jogo_bola.cpp
+
+${OBJECTDIR}/lobo.o: nbproject/Makefile-${CND_CONF}.mk lobo.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/lobo.o lobo.cpp
+
+${OBJECTDIR}/jogo_morcego.o: nbproject/Makefile-${CND_CONF}.mk jogo_morcego.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/jogo_morcego.o jogo_morcego.cpp
+
+${OBJECTDIR}/gota.o: nbproject/Makefile-${CND_CONF}.mk gota.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/gota.o gota.cpp
+
+${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/jogo_coruja.o: nbproject/Makefile-${CND_CONF}.mk jogo_coruja.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/jogo_coruja.o jogo_coruja.cpp
+
+${OBJECTDIR}/rato.o: nbproject/Makefile-${CND_CONF}.mk rato.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/rato.o rato.cpp
+
+${OBJECTDIR}/animal.o: nbproject/Makefile-${CND_CONF}.mk animal.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/animal.o animal.cpp
+
+${OBJECTDIR}/atoxado.o: nbproject/Makefile-${CND_CONF}.mk atoxado.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/atoxado.o atoxado.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/Release
+	${RM} dist/Release/GNU-Linux-x86/torquere
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
